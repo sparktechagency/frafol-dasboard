a9a4cc37143c56cf3e1ed9cccc58d3cc9a5a6886
rajin572 <dirrazin572@gmail.com>
2025-09-24 12:43:53 +0600

    Implement commission setup management with API integration and form updates

--- src/pages/Admin/AdminCommissionSetupPage.tsx
+++ src/pages/Admin/AdminCommissionSetupPage.tsx
@@ -3,37 +3,79 @@ import { Form } from "antd";
 import ReusableForm from "../../ui/Form/ReuseForm";
 import ReuseInput from "../../ui/Form/ReuseInput";
 import ReuseButton from "../../ui/Button/ReuseButton";
+import { MdOutlineEuro, MdOutlinePercent } from "react-icons/md";
+import {
+  useGetCommissionSetupQuery,
+  useUpdateCommissionSetupMutation,
+} from "../../redux/features/commissionSetup/commissionSetupApi";
+import Loading from "../../ui/Loading";
+import React from "react";
+import tryCatchWrapper from "../../utils/tryCatchWrapper";
 
 const inputStructure = [
   {
-    name: "professionalCommissionPercentage",
-    label: "Set Commission For Photography/Videography Orders",
+    name: "photoVideoGrapy",
+    label: "Set Commission Rate For Photography/Videography Orders",
+    inputType: "number",
     placeholder: "Enter your commission percentage",
     labelClassName: "!font-medium",
     rules: [{ required: true, message: "Commission percentage is required" }],
+    prefix: <MdOutlinePercent className="text-secondary-color" />,
   },
   {
-    name: "gearCommissionPercentage",
-    label: "Set Commission For Gear Orders",
+    name: "minimumCharge",
+    label: "Set Minimum Commission Amount For Photography/Videography Orders",
+    inputType: "number",
     placeholder: "Enter your commission percentage",
     labelClassName: "!font-medium",
     rules: [{ required: true, message: "Commission percentage is required" }],
+    prefix: <MdOutlineEuro className="text-secondary-color" />,
   },
   {
-    name: "workshopCommissionPercentage",
-    label: "Set Commission For Workshop",
+    name: "gearOrders",
+    label: "Set Commission  Rate For Gear Orders",
+    inputType: "number",
+    placeholder: "Enter your commission percentage",
+    labelClassName: "!font-medium",
+    rules: [{ required: true, message: "Commission percentage is required" }],
+    prefix: <MdOutlinePercent className="text-secondary-color" />,
+  },
+  {
+    name: "workShop",
+    label: "Set Commission  Rate For Workshop",
+    inputType: "number",
     placeholder: "Enter your commission percentage",
     labelClassName: "!font-medium",
     rules: [{ required: true, message: " Commission percentage is required" }],
+    prefix: <MdOutlinePercent className="text-secondary-color" />,
   },
 ];
 
 const AdminCommissionSetupPage = () => {
   const [form] = Form.useForm();
+
+  const { data, isFetching } = useGetCommissionSetupQuery({});
+  const [updateCommissionSetup] = useUpdateCommissionSetupMutation();
+
+  const commissionSetupData = data?.data;
+
+  React.useEffect(() => {
+    if (commissionSetupData) {
+      form.setFieldsValue(commissionSetupData);
+    }
+  }, [commissionSetupData, form]);
+
   const handleFinish = (values: any) => {
-    console.log("Form values:", values);
-    // Handle form submission logic here
+    tryCatchWrapper(
+      updateCommissionSetup,
+      { body: values },
+      "Updating Commission Setup..."
+    );
   };
+
+  if (isFetching) {
+    return <Loading />;
+  }
   return (
     <div className="min-h-[90vh]">
       <div className="flex justify-between items-center mx-3 py-2 mb-5 ">
@@ -44,12 +86,15 @@ const AdminCommissionSetupPage = () => {
       <ReusableForm form={form} handleFinish={handleFinish}>
         {inputStructure.map((input, index) => (
           <ReuseInput
+            type="number"
             key={index}
             name={input.name}
             label={input.label}
+            inputType={input.inputType}
             placeholder={input.placeholder}
             labelClassName={input.labelClassName}
             rules={input.rules}
+            prefix={input.prefix}
           />
         ))}
         <ReuseButton
