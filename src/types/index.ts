f7d8614b206a30c5355ae6e3197e939516306c09
rajin572 <dirrazin572@gmail.com>
2025-10-20 17:31:11 +0600

    Order Management order For Event

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,4 @@ export * from "./feedback.type";
 export * from "./gear.type";
 export * from "./package.type";
 export * from "./workshop.type";
+export * from "./eventOrder.type";
---
+++ src/utils/budgetLabels.tsx
@@ -0,0 +1,17 @@
+export const budgetLabels: { [key: string]: string } = {
+  under500: "Under 500€",
+  "500-1000": "500€-1000€",
+  "1000-2000": "1000€-2000€",
+  "2000-5000": "2000€-5000€",
+  over5000: "Over 5000€",
+};
+export const eventOrderStatus: { [key: string]: string } = {
+  pending: "Pending",
+  declined: "Declined",
+  accepted: "Waiting for Payment",
+  inProgress: "In Progress",
+  deliveryRequestDeclined: "In Progress",
+  deliveryRequest: "Delivery Request",
+  delivered: "Delivered",
+  cancelled: "Cancelled",
+};
